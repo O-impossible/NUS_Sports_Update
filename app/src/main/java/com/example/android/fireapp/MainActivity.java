037e1cc262a576b259cdb8e1226bba3e9635d6b1
Yash Chowdhary <yashchowdhary98@gmail.com>
2017-06-12 14:52:06 +0530

    commit 3

--- app/src/main/java/com/example/android/fireapp/MainActivity.java
+++ app/src/main/java/com/example/android/fireapp/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+//override
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
