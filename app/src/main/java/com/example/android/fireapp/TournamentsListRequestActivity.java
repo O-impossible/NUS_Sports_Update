66586d23a19637cc9468fc44c703db5b8bc825e5
Yash Chowdhary <yashchowdhary98@gmail.com>
2017-06-23 21:28:43 +0530

    commit 29 - 1. centered the team names in the fixture display

--- app/src/main/java/com/example/android/fireapp/TournamentsListRequestActivity.java
+++ app/src/main/java/com/example/android/fireapp/TournamentsListRequestActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
