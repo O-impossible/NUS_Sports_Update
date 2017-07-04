e4c5cd7c2fc1a3c4c7f8eb026f1a9f106004f6f3
Yash Chowdhary <yashchowdhary98@gmail.com>
2017-07-04 22:14:26 +0530

    commit 37 - 1. chat feature working perfectly

--- app/src/main/java/com/example/android/fireapp/ChatMessageAdapter.java
+++ app/src/main/java/com/example/android/fireapp/ChatMessageAdapter.java
@@ -1,8 +1,64 @@
 package com.example.android.fireapp;
 
+import android.app.Activity;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
 /**
  * Created by Yash Chowdhary on 04-07-2017.
  */
 
-public class ChatMessageAdapter {
+public class ChatMessageAdapter extends ArrayAdapter<ChatMessage>{
+    private DatabaseReference lockerroomRef;
+
+    private TextView userView;
+    private TextView timeView;
+    private TextView messageView;
+    private ArrayList<ChatMessage> mMessagesList;
+
+    public ChatMessageAdapter(Activity context, ArrayList<ChatMessage> messages){
+        super(context,0,messages);
+        mMessagesList = messages;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listViewItem = convertView;
+        if(listViewItem == null) {
+            listViewItem = LayoutInflater.from(getContext()).inflate(
+                    R.layout.message_list_item, parent, false);
+        }
+
+        userView = (TextView) listViewItem.findViewById(R.id.message_user);
+        timeView = (TextView) listViewItem.findViewById(R.id.message_time);
+        messageView = (TextView) listViewItem.findViewById(R.id.message_text);
+
+        final ChatMessage message = getItem(position);
+
+        //updateAdapter(mMessagesList);
+        userView.setText(message.getMessageUser());
+        timeView.setText(Long.toString(message.getMessageTime()));
+        messageView.setText(message.getMessageText());
+
+
+        return listViewItem;
+    }
+
+    private synchronized void updateAdapter(ArrayList<ChatMessage> messageArrayList) {
+        mMessagesList.clear();
+        mMessagesList.addAll(messageArrayList);
+        notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/example/android/fireapp/LockerRoomActivity.java
+++ app/src/main/java/com/example/android/fireapp/LockerRoomActivity.java
@@ -3,15 +3,26 @@ package com.example.android.fireapp;
 import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -24,8 +35,12 @@ public class LockerRoomActivity extends AppCompatActivity {
     private String userName;
     private String faculty;
 
+    RelativeLayout activity_lockerroom;
+
     private EditText messageInput;
     private FloatingActionButton sendFAB;
+
+    private FirebaseListAdapter<ChatMessage> adapter;
 
     private DatabaseReference mDatabase;
 
@@ -53,33 +68,111 @@ public class LockerRoomActivity extends AppCompatActivity {
         Log.d("faculty:",faculty);
         Log.d("sport:",sportName);
         Log.d("checking string value:",getString(R.string.lockerroom));
-        mDatabase = FirebaseDatabase.getInstance().getReference().child(getString(R.string.lockerroom));
+
+        setTitle(faculty + "'s " + "LockerRoom - "+ sportName);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child(getString(R.string.lockerroom)).child(tournamentId).
+                child(sportName).child(faculty);
         messageInput = (EditText) findViewById(R.id.message_input);
         sendFAB = (FloatingActionButton) findViewById(R.id.send_fab);
 
-        populateView();
+        activity_lockerroom = (RelativeLayout) findViewById(R.id.activity_lockerroom);
+
+        View emptyView = findViewById(R.id.empty_view_no_messages);
+        ListView listOfMessage = (ListView)findViewById(R.id.list_of_messages);
+        listOfMessage.setEmptyView(emptyView);
+
+        displayChat();
     }
 
-    private void populateView() {
+    private void displayChat() {
+        ListView listOfMessage = (ListView)findViewById(R.id.list_of_messages);
+        adapter = new FirebaseListAdapter<ChatMessage>(this,ChatMessage.class,R.layout.message_list_item,mDatabase)
+        {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
 
+                //Get references to the views of list_item.xml
+                TextView messageText, messageUser, messageTime;
+                messageText = (TextView) v.findViewById(R.id.message_text);
+                messageUser = (TextView) v.findViewById(R.id.message_user);
+                messageTime = (TextView) v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+
+            }
+        };
+        listOfMessage.setAdapter(adapter);
     }
+
+    /*private void populateView() {
+        final ArrayList<ChatMessage> chatMessageArrayList = new ArrayList<>();
+
+        final ListView messagesListView = (ListView) findViewById(R.id.list_of_messages);
+
+        mDatabase.child(tournamentId).child(sportName).child(faculty).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot message:dataSnapshot.getChildren()){
+                        ChatMessage retrievedMessage = message.getValue(ChatMessage.class);
+                        chatMessageArrayList.add(retrievedMessage);
+                    }
+                }
+
+                ChatMessageAdapter chatMessageAdapter = new ChatMessageAdapter(LockerRoomActivity.this,chatMessageArrayList);
+
+                View emptyView = findViewById(R.id.empty_view_no_messages);
+
+                messagesListView.setAdapter(chatMessageAdapter);
+
+                if(chatMessageArrayList.size()==0)
+
+                {
+                    emptyView.setVisibility(View.VISIBLE);
+                    messagesListView.setVisibility(View.GONE);
+                }
+
+                else
+
+                {
+                    emptyView.setVisibility(View.GONE);
+                    messagesListView.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }*/
+
 
     private void callFAB() {
+
         sendFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
                 String message = messageInput.getText().toString().trim();
+                if(TextUtils.isEmpty(message)){
+                    Snackbar.make(activity_lockerroom,"Please enter a message",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
                 ChatMessage chatMessage = new ChatMessage(message,userName);
                 HashMap<String, Object> chatMessageHashMap = new HashMap<String,Object>();
                 chatMessageHashMap.put("messageText",message);
                 chatMessageHashMap.put("messageUser",userName);
                 chatMessageHashMap.put("messageTime",chatMessage.getMessageTime());
-                DatabaseReference pushedRef = mDatabase.child(tournamentId).child(sportName).child(faculty).push();
+                DatabaseReference pushedRef = mDatabase.push();
                 pushedRef.updateChildren(chatMessageHashMap);
 //                mDatabase.child(Integer.toString(R.string.lockerroom)).child(tournamentId).child(sportName).
 //                        child(faculty).updateChildren(chatMessageHashMap);
                 messageInput.setText("");
+                displayChat();
             }
         });
     }
